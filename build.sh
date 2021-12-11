555eb4393b4242fc559c9cd1ae143e77734dfbce
Felix Zieger <mail@felixzieger.de>
2021-12-11 20:17:09 +0100

    more platforms

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ compile(){
   deno compile $deno_flags --target "$target" --output "./bin/pom-$target" src/main.ts
 }
 
-#compile "x86_64-unknown-linux-gnu"
+compile "x86_64-unknown-linux-gnu"
 compile "x86_64-apple-darwin"
-#compile "x86_64-pc-windows-msvc"
+compile "x86_64-pc-windows-msvc"
 
 # first grep filters for the first line of the output "deno 1.8.1 (release, x86_64-apple-darwin)"
 # second grep extracts the architecture string, but can't get rid of the trailing ): "x86_64-apple-darwin)"
